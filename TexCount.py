cf61918b5b0b6801e8070dda0d1033b7f569a3ae
Ben Sowden <look.bens@gmail.com>
2013-04-04 15:40:01 +0300

    added saving and comments

--- TexCount.py
+++ TexCount.py
@@ -5,28 +5,34 @@ class TexcountCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		filename = self.view.file_name()
 
+		# Check a file is selected
 		if filename == None:
 			sublime.error_message("No file selected")
 			return
 
+		# Save if file has been edited since last save
+		if (self.view.is_dirty()):
+			self.view.run_command('save')
+
+		# Cleanse name and make shell command
 		filename = filename.replace(" ","\ ")
 		cmd = "texcount " + filename
 
 		# MacTex fix
 		cmd = "PATH=$PATH:/usr/texbin; " + cmd
 
+		# Test to see if texcount is installed and in available PATH
 		testcmdprocess = Popen("PATH=$PATH:/usr/texbin; which texcount", shell=True, stdout=PIPE, stderr=PIPE)
 		testout, testerr = testcmdprocess.communicate()
 		if (testout == ""):
 			sublime.error_message("TeXcount not installed in PATH \nDownload from: http://app.uio.no/ifi/texcount/")
 			return
 
+		# Excecute texcount and collect output
 		p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
 		out, err = p.communicate()
 
-		if (self.view.is_dirty()):
-			out = out + "\nWarning, File not up to date!"
-
+		# Display Sublime console and print texcount output to console
 		sublime.active_window().run_command("show_panel", {"panel": "console", "toggle": True})
 		out = out.strip()
 		print(out)
